61ff69d8ae2548bc10021243d08910482f54b00b
Aleena Joseph <ec25742@qmul.ac.uk>
2026-05-17 16:31:17 +0100

    removing inactive, adding helper methods to enum for safer id status management

--- src/main/java/com/qmul/digitalid/model/DigitalIDStatus.java
+++ src/main/java/com/qmul/digitalid/model/DigitalIDStatus.java
@@ -1,8 +1,32 @@
 package com.qmul.digitalid.model;
 
+import java.awt.*;
+
 public enum DigitalIDStatus {
     ACTIVE,
-    INACTIVE,
     SUSPENDED,
-    REVOKED
+    REVOKED;
+
+    public boolean isUsableByConsumers() {
+        return this == ACTIVE;
+    }
+
+    public boolean canBeUpdated() {
+        return this == ACTIVE || this == SUSPENDED;
+    }
+
+    public boolean canBeSuspended() {
+        return this == ACTIVE;
+    }
+
+    public boolean canBeReactivated() {
+        return this == SUSPENDED;
+    }
+
+    public boolean canBeRevoked() {
+        return this == REVOKED;
+    }
+
+
 }
+
