a5cd8863a7f196a6544e87782e38a925a43b9cff
Aleena Joseph <ec25742@qmul.ac.uk>
2026-05-18 15:36:46 +0100

    implement digital ID creation

--- src/main/java/com/qmul/digitalid/service/IdentityManagementServiceImpl.java
+++ src/main/java/com/qmul/digitalid/service/IdentityManagementServiceImpl.java
@@ -4,6 +4,9 @@ import com.qmul.digitalid.exception.DigitalIdNotFoundException;
 import com.qmul.digitalid.model.DigitalID;
 import com.qmul.digitalid.repository.DigitalIdRepository;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class IdentityManagementServiceImpl implements IdentityManagementService {
 
     private final DigitalIdRepository repository;
@@ -13,6 +16,55 @@ public class IdentityManagementServiceImpl implements IdentityManagementService 
                                          LogService logService) {
         this.repository = repository;
         this.logService = logService;
+    }
+
+    @Override
+    public DigitalID createIdentity(String nationalIdNumber, String firstName, String lastName, LocalDate dateOfBirth, String address, String nationality, String requestedBy) {
+        String id = UUID.randomUUID().toString();
+
+        DigitalID digitalID = new DigitalID(
+                id,
+                nationalIdNumber,
+                firstName,
+                lastName,
+                dateOfBirth,
+                address,
+                nationality
+        )
+
+        repository.save(digitalID);
+
+        return digitalID;
+    }
+
+    @Override
+    public void updateFirstName(String id, String newFirstName, String requestedBy) {
+
+    }
+
+    @Override
+    public void updateLastName(String id, String newLastName, String requestedBy) {
+
+    }
+
+    @Override
+    public void updateAddress(String id, String newAddress, String requestedBy) {
+
+    }
+
+    @Override
+    public void suspendIdentity(String id, String requestedBy) {
+
+    }
+
+    @Override
+    public void reactivateIdentity(String id, String requestedBy) {
+
+    }
+
+    @Override
+    public void revokeIdentity(String id, String requestedBy) {
+
     }
 
     @Override
