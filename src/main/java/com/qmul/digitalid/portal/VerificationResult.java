edf42bb4374f5d2103e1ce402b1e010f8ed0672a
Aleena Joseph <ec25742@qmul.ac.uk>
2026-05-19 13:55:35 +0100

    remove redundant toString method

--- src/main/java/com/qmul/digitalid/portal/VerificationResult.java
+++ src/main/java/com/qmul/digitalid/portal/VerificationResult.java
@@ -1,12 +1,3 @@
 package com.qmul.digitalid.portal;
 
-public record VerificationResult(boolean valid, String reason) {
-
-    @Override
-    public String toString() {
-        return "VerificationResult{" +
-                "valid=" + valid +
-                ", reason='" + reason + '\'' +
-                '}';
-    }
-}
+public record VerificationResult(boolean valid, String reason) {}
