f53fed927e463b3a8c0d6798aae55d8ac73da67a
Aleena Joseph <ec25742@qmul.ac.uk>
2026-05-19 13:56:16 +0100

    remove comment i needed previously

--- src/main/java/com/qmul/digitalid/exception/DigitalIdNotFoundException.java
+++ src/main/java/com/qmul/digitalid/exception/DigitalIdNotFoundException.java
@@ -1,6 +1,5 @@
 package com.qmul.digitalid.exception;
 
-//meaningful exceptions
 public class DigitalIdNotFoundException extends RuntimeException {
     public DigitalIdNotFoundException(String id) {
         super("Digital ID not found: " + id);
