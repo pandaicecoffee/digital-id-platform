988a898c45f80908a809f2d69cc490f9c2b218c4
Aleena Joseph <ec25742@qmul.ac.uk>
2026-05-19 10:30:12 +0100

    extending interface + lookup method skeleton

--- src/main/java/com/qmul/digitalid/service/IdentityConsumptionImpl.java
+++ src/main/java/com/qmul/digitalid/service/IdentityConsumptionImpl.java
@@ -1,14 +1,34 @@
 package com.qmul.digitalid.service;
 
 
+import com.qmul.digitalid.model.DigitalID;
+import com.qmul.digitalid.portal.VerificationResult;
 import com.qmul.digitalid.repository.DigitalIdRepository;
 
-public class IdentityConsumptionImpl {
+import java.time.LocalDate;
+import java.util.Optional;
+
+public class IdentityConsumptionImpl extends IdentityConsumptionService{
     private final DigitalIdRepository repository;
     private final LogService logService;
 
     public IdentityConsumptionImpl(DigitalIdRepository repository, LogService logService) {
         this.repository = repository;
         this.logService = logService;
+    }
+
+    @Override
+    public VerificationResult verifyIsActive(String digitalIdRef, String requestedBy) {
+        return null;
+    }
+
+    @Override
+    public VerificationResult verifyActiveForPeriod(String digitalIdRef, LocalDate from, LocalDate to, String requestedBy) {
+        return null;
+    }
+
+    @Override
+    public Optional<DigitalID> lookup(String digitalIdRef, String requestedBy) {
+        return null;
     }
 }
